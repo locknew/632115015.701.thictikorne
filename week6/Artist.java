6160343569bdba2b1af2db0f91ef2001dea9e240
locknew <45910721+locknew@users.noreply.github.com>
2021-01-13 15:45:26 +0700

    m

---
+++ week6/Artist.java
@@ -0,0 +1,13 @@
+package week6;
+
+public class Artist extends Person{
+    String genre;
+    public Artist(String name,int age){
+        this.age = age;
+        this.name = name;
+    }
+    public void playMusic(){
+        System.out.println(name + " is playing ");
+    }
+
+}
--- week6/Person.java
+++ week6/Person.java
@@ -16,15 +16,5 @@ public class Person {
         System.out.println("My name is  " + name);
 
     }
-    public class Artist extends Person{
-        String genre;
-        public Artist(String name,int age){
-            this.age = age;
-            this.name = name;
-        }
-        public void playMusic(){
-            System.out.println(name + " is playing ")
-        }
-
-    }
+   
 }
